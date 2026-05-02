7dad723a12ffb3f563591391aa4ce436892ca140
Jules van Dommelen <julesvdommelen@gmail.com>
2026-05-02 14:10:17 +0200

    add new scenarios to scenario engine

--- backend/scenarios/scenario_engine.py
+++ backend/scenarios/scenario_engine.py
@@ -1,8 +1,18 @@
 from .coffee_ordering import SCENARIO
+from .doctor_visit import SCENARIO
+from .hotel_check_in import SCENARIO
+from .job_interview import SCENARIO
+from .store_return import SCENARIO
+from .tourist_information import SCENARIO
 from .prompt_builder import build_prompt
 
 SCENARIOS = {
-    "coffee_ordering": SCENARIO
+    "coffee_ordering": SCENARIO,
+    "doctor_visit": SCENARIO,
+    "hotel_check_in": SCENARIO,
+    "job_interview": SCENARIO,
+    "store_return": SCENARIO,
+    "tourist_information": SCENARIO,
 }
 
 def create_prompt(state, user_message):

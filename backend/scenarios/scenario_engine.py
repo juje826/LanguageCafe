57d910906b2fe5dd385b4418dd442a134b60d3bb
Jules van Dommelen <julesvdommelen@gmail.com>
2026-05-09 14:36:07 +0200

    fix scenario selection bug

--- backend/scenarios/scenario_engine.py
+++ backend/scenarios/scenario_engine.py
@@ -1,18 +1,18 @@
-from .coffee_ordering import SCENARIO
-from .doctor_visit import SCENARIO
-from .hotel_check_in import SCENARIO
-from .job_interview import SCENARIO
-from .store_return import SCENARIO
-from .tourist_information import SCENARIO
+from . import coffee_ordering
+from . import doctor_visit
+from . import hotel_check_in
+from . import job_interview
+from . import store_return
+from . import tourist_information
 from .prompt_builder import build_prompt
 
 SCENARIOS = {
-    "coffee_ordering": SCENARIO,
-    "doctor_visit": SCENARIO,
-    "hotel_check_in": SCENARIO,
-    "job_interview": SCENARIO,
-    "store_return": SCENARIO,
-    "tourist_information": SCENARIO,
+    "coffee_ordering": coffee_ordering.SCENARIO,
+    "doctor_visit": doctor_visit.SCENARIO,
+    "hotel_check_in": hotel_check_in.SCENARIO,
+    "job_interview": job_interview.SCENARIO,
+    "store_return": store_return.SCENARIO,
+    "tourist_information": tourist_information.SCENARIO,
 }
 
 def create_prompt(state, user_message):
